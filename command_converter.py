36390acc4a69ca00edfd4ac7fa4601f5a93b7b1f
Alexey Gribchenko <gribchenko.2003@mail.ru>
2025-10-02 11:59:46 +0300

    Initial commit. Work in progress.

---
+++ command_converter.py
@@ -0,0 +1,139 @@
+COMMANDS = {
+    'EMPTY': 0b0000,
+    'LOAD': 0b0001,
+    'STORE': 0b0010,
+    'INC': 0b0100,
+    'DEC': 0b0101,
+    'JP': 0b0110,
+    'JN': 0b0111,
+    'CMP': 0b1000,
+    'RET': 0b1111
+}
+
+class Assembler:
+
+    def __init__(self, filename):
+        self.points = {}
+        self.programm = []
+        self.filename = filename
+
+    def parse_operand(self, operand):
+        """
+        Returns value, is_register, is_address.
+        
+        value - scalar value or memory address or register number.
+        
+        is_register - indicates if operand register or not.
+        
+        is_address - indicates whether to work with the memoty or with the value.
+        """
+
+
+        operand = operand.strip()
+        is_address = '[' in operand
+        
+        clean_operand = operand.replace('[', '').replace(']', '') if is_address else operand
+        is_register = clean_operand.startswith('R') and clean_operand[1:].isdigit()
+
+        reg_number = clean_operand[1:] if is_register else clean_operand
+
+        try:
+            value = int(reg_number)
+        except ValueError:
+            value = reg_number
+        
+        return value, is_register, is_address
+        
+    def first_pass(self):
+        """
+        First pass. Collecting marks.
+        Example:
+
+            MARK:
+        """
+
+        with open(self.filename, 'r') as file:
+            command_index = 0
+
+            for line in file:
+                words = line.strip().split()
+                
+                if len(words) == 0:
+                    continue
+
+                if len(words) == 1 and words[0].endswith(':'):
+                    label = words[0][:-1]
+                    self.points[label] = command_index
+                elif words[0] in COMMANDS:
+                    command_index += 1
+        
+    def second_pass(self):
+        """
+        Second pass. Machine code generation.
+        Example:
+
+            LOAD [R1] -> 0b00011100000000001
+        """
+
+        with open(self.filename, 'r') as file:
+            command_insex = 0
+
+            for line in file:
+                words = line.strip().split()
+
+                if len(words) == 0:
+                    continue
+
+                if len(words) == 1 and words[0].endswith(':'):
+                    command_insex += 1
+                    continue
+
+                if words[0] not in COMMANDS:
+                    continue
+                    
+                command = words[0]
+                cmd_code = COMMANDS[command] << 12
+                val = 0
+                reg = 0
+                ad = 0
+                
+                if len(words) == 1:
+                    result = cmd_code
+                
+                elif len(words) == 2:
+                    operand = words[1]
+
+                    if command in ['JP', 'JN']:
+                        if operand not in self.points:
+                            raise ValueError(f'Error! Wrong operand value!\ncommand: {command}\nvalue: {operand}')
+                        val = self.points[operand]
+                    else:
+                        val, reg, ad = self.parse_operand(operand)
+                        # FOR WORK WITH DATA
+                        # if isinstance(val, str) and val in seld.data:
+                        #     val = self.data[val]
+                    
+                    result = cmd_code | reg << 11 | ad << 10 | val
+                
+                else:
+                    raise ValueError('Error! Invalid syntax:', line)
+                
+                # print(f'self.cmem[{command_insex}] = {bin(result)} {words[0]}')
+
+                self.programm.append(result)
+                command_insex += 1
+    
+    def print_commands(self):
+
+        for i, command in enumerate(self.programm):
+            print(f'self.cmem[{i}] = {bin(command)}')
+
+    def assemble(self):
+        self.first_pass()
+        self.second_pass()
+
+
+if __name__ == "__main__":
+        
+    a = Assembler('programm.txt')
+    a.assemble()
---
+++ main.py
@@ -0,0 +1,228 @@
+# hello everyone today we gonna learn c++
+
+from command_converter import Assembler
+
+'''
+    commands:
+    
+    0x0000 EMPTY - do nothing
+    0x0001 LOAD  - load data into ACC from operand
+    0x0010 STORE - load data from ACC into operand
+    0x0100 INC   - increment operand
+    0x0101 DEC   - decrement operand
+    0x0110 JP    - jump to istruction if result of comparement if positive
+    0x0111 JN    - jump to istruction if result of comparement if negative
+    0x1000 CMP   - compare ACC and operand values
+    0x1111 RET   - stop the programm
+
+    comand:
+
+    |-------------|-------------|--------------|---------|
+    |    15-12    |     11      |      10      |   9-0   |
+    |-------------|-------------|--------------|---------|
+    | comand type | is register |  is address  |  value  |
+    |-------------|-------------|--------------|---------|
+
+'''
+
+class Emulator:
+
+    def __init__(self):
+        # registers
+        self.acc = 0
+        self.pc = 0
+        self.RON = [0] * 16
+        # memory
+        self.cmem = [0] * 32
+        self.dmem = [0] * 16
+        # flags
+        self.ez = 0
+        self.sf = 0
+    
+    def __separate_address(self, operand):
+        is_reg = (operand >> 11) & 1
+        is_bracket = (operand >> 10) & 1
+        value = operand & 0x3FF
+        
+        return is_reg, is_bracket, value
+
+    def __handle_command(self, command, operand):
+
+        match command:
+            case 0b0000:
+                """EMPTY"""
+
+                pass
+            case 0b0001:
+                """LOAD"""
+
+                is_reg, is_bracket, val = self.__separate_address(operand)
+
+                if not is_bracket and not is_reg:
+                    self.acc = val
+                    return
+                
+                if is_reg and is_bracket:
+                    self.acc = self.dmem[self.RON[val]]
+                    return
+                
+                if is_bracket:
+                    self.acc = self.dmem[val]
+                    return
+                
+                if is_reg:
+                    self.acc = self.RON[val]
+                    return
+            
+            case 0b0010:
+                """STORE"""
+
+                is_reg, is_bracket, val = self.__separate_address(operand)
+
+                if not is_bracket and not is_reg:
+                    print("Error! Can not load value into scalar!")
+                    exit(1)
+
+                if is_reg and is_bracket:
+                    self.RON[val] = self.dmem[self.acc]
+                    return
+                elif is_reg and not is_bracket:
+                    self.RON[val] = self.acc
+                else:
+                    self.dmem[val] = self.acc
+
+            case 0b0100:
+                """
+                INC
+                
+                Increments value in register or in memory. Does not increment scalars!
+                """
+
+                is_reg, is_bracket, val = self.__separate_address(operand)
+
+                if not is_reg and not is_bracket:
+                    raise ValueError("Error! Can not increment scalar!")
+
+                if is_reg and is_bracket:
+                    self.dmem[self.RON[val]] += 1
+                    return
+                
+                if is_reg:
+                    self.RON[val] += 1
+                    return
+                
+                if is_bracket:
+                    self.dmem[val] += 1
+                    return
+            
+            case 0b0101:
+                """
+                DEC
+                
+                Decrements value in register or in memory. Does not decrement scalars!
+                """
+
+                is_reg, is_bracket, val = self.__separate_address(operand)
+
+                if not is_reg and not is_bracket:
+                    raise ValueError("Error! Can not increment scalar!")
+
+                if is_reg and is_bracket:
+                    self.dmem[self.RON[val]] -= 1
+                    return
+                
+                if is_reg:
+                    self.RON[val] -= 1
+                    return
+                
+                if is_bracket:
+                    self.dmem[val] -= 1
+                    return
+            
+            case 0b0110:
+                """JP"""
+
+                if self.ez == 0 and self.sf == 0:
+                    self.pc = operand
+                    self.pc -= 1 # compensation for pc += 1 in "programm loop"
+                else:
+                    pass
+
+            case 0b0111:
+                """JN"""
+
+                if self.ez == 0 and self.sf == 1:
+                    self.pc = operand
+                    self.pc -= 1 # compensation for pc += 1 in "programm loop"
+                else:
+                    pass
+            
+            case 0b1000:
+                """CMP"""
+
+                is_reg, is_bracket, val = self.__separate_address(operand)
+
+                subtrahend = operand
+
+                if is_reg and is_bracket:
+                    subtrahend = self.dmem[self.RON[val]]
+                elif is_reg:
+                    subtrahend = self.RON[val]
+                elif is_bracket:
+                    subtrahend = self.dmem[val]
+
+                diff = self.acc - subtrahend
+                if diff > 0:
+                    self.ez = 0
+                    self.sf = 0
+                elif diff == 0:
+                    self.ez = 1
+                    self.sf = 0
+                else:
+                    self.ez = 0
+                    self.sf = 1
+            
+            case 0b1111:
+                pass
+
+            case _:
+                print("Error, wrong comand value!")
+                exit(1)
+    
+    def run_emulator(self, assembler: Assembler):
+
+        cmd = 0
+        ad = 0
+
+        self.dmem[0] = 7
+        self.dmem[1:8] = [129, -321, 23, 1919, 2, 0, -9]
+
+        assembler.assemble()
+        
+        self.cmem[0:len(assembler.programm)] = assembler.programm[:]
+
+        # "programm loop"
+        while cmd != 0b1111:
+    
+            cmd = self.cmem[self.pc] >> 12
+            ad = self.cmem[self.pc] & 0xFFF
+
+            print(f'Command: {bin(cmd)}')
+            self.__handle_command(cmd, ad)
+
+            self.pc += 1
+
+        print(self.RON, self.acc)
+
+
+'''
+заметка на будущее
+если надо будет сохранять в память данные из секции .data, то можно сделать словарь с
+адресами, наверно
+'''
+
+if __name__ == "__main__":
+
+    a = Assembler('programm.txt')
+    e = Emulator()
+    e.run_emulator(a)
